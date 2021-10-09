b065829309fa0a1c93228826b4b934043a1dbcbf
varnikak00 <varnikak7070@gmail.com>
2021-10-09 17:41:42 +0530

    regex

---
+++ username/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace username
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To RegEx");
+            ValidateFirstName fn = new ValidateFirstName();
+            Console.WriteLine("Enter The FirstName :");
+            String FirstName = Console.ReadLine();
+            bool result = fn.firstNameCheck(FirstName);
+            if (result == true)
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+        }
+    }
+}
