fbb298093655e6bb910f4d22d31ffd1bd65badc8
varnikak00 <varnikak7070@gmail.com>
2021-10-10 14:21:49 +0530

    validatepasword

--- username/Program.cs
+++ username/Program.cs
@@ -6,18 +6,18 @@ namespace username
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To RegEx");
+            
             ValidateFirstName fn = new ValidateFirstName();
             Console.WriteLine("Enter The FirstName :");
             String FirstName = Console.ReadLine();
             bool result = fn.firstNameCheck(FirstName);
             if (result == true)
             {
-                Console.WriteLine("valid");
+                Console.WriteLine("true");
             }
             else
             {
-                Console.WriteLine("Invalid");
+                Console.WriteLine("false");
             }
 
         }
